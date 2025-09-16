e12ac76502749f0c760dedb8a9afd349e7c594cb
Shashanktk05 <shashanktk510@gmail.com>
2025-09-17 00:02:47 +0530

    weather application using api

---
+++ Basicss/Weather App/script.js
@@ -0,0 +1,51 @@
+const apiKey="6a594afc92cd5c2c8faf302e75848789";
+
+const apiUrl="https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
+const searchBox=document.querySelector(".search input");
+const searchBtn=document.querySelector(".search button");
+const weatherIcon=document.querySelector(".weather-icon");
+
+async function checkWeather(city){
+
+
+    const response=await fetch(apiUrl+city+`&appid=${apiKey}`);
+
+    if(response.status==404){
+        document.querySelector(".error").style.display="block";
+        document.querySelector(".weather").style.display="none";
+    }
+    else{
+        var data=await response.json();
+    document.querySelector(".city").innerHTML=data.name;
+    document.querySelector(".temp").innerHTML=Math.round(data.main.temp)+"°C";
+    document.querySelector(".humidity").innerHTML=data.main.humidity+"%";
+    document.querySelector(".wind").innerHTML=data.wind.speed+"km/h";   
+
+    if(data.weather[0].main=="Clouds"){
+        weatherIcon.src= "assets/images/clouds.png";
+    }
+    if(data.weather[0].main=="Clear"){
+        weatherIcon.src= "assets/images/clear.png";
+    }
+    if(data.weather[0].main=="Drizzle"){
+        weatherIcon.src= "assets/images/drizzle.png";
+    }
+    if(data.weather[0].main=="Mist"){
+        weatherIcon.src= "assets/images/mist.png";
+    }
+    if(data.weather[0].main=="Rain"){
+        weatherIcon.src= "assets/images/rain.png";
+    }
+    if(data.weather[0].main=="Snow"){
+        weatherIcon.src= "assets/images/snow.png";
+    }
+    
+    document.querySelector(".weather").style.display="block";
+    document.querySelector(".error").style.display="none";
+}
+    
+}
+
+searchBtn.addEventListener("click",()=>{
+checkWeather(searchBox.value);
+});
