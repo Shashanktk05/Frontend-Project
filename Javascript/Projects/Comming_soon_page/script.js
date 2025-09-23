aff9135f841d6148f1c6165e09296110aae282bb
Shashanktk05 <shashanktk510@gmail.com>
2025-09-23 20:31:29 +0530

    lanching page

---
+++ Javascript/Projects/Comming_soon_page/script.js
@@ -0,0 +1,11 @@
+let countDownDate=new Date("Oct 5,2025 00:00:00").getTime();
+let x=setInterval(function(){
+    let now=new Date().getTime();
+    let distance=countDownDate-now;
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+
+},1000);
