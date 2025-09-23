efeaa734d5414e67cd3644975c6a0f5f6fc221b4
Shashanktk05 <shashanktk510@gmail.com>
2025-09-23 21:17:51 +0530

    added script and finished the project

--- Javascript/Projects/Comming_soon_page/script.js
+++ Javascript/Projects/Comming_soon_page/script.js
@@ -6,6 +6,18 @@ let x=setInterval(function(){
     let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     let seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
+    document.getElementById('sec').innerHTML=seconds
+    document.getElementById('min').innerHTML=minutes
+    document.getElementById('hours').innerHTML=hours
+    document.getElementById('days').innerHTML=days
+
+    if(distance<0){
+        clearInterval(x);
+        document.getElementById('sec').innerHTML="00"
+        document.getElementById('min').innerHTML="00"
+        document.getElementById('hours').innerHTML="00"
+        document.getElementById('days').innerHTML="00"
+    }
 
 },1000);
+
